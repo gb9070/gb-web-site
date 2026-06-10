50799c53742e9e3109ae68cd4953dec561f10407
Minecat <chrisdanriviere@gmail.com>
2026-06-10 19:11:22 +0200

    fixed and added css for single case

--- gb-web-site/src/Components/singleCase.jsx
+++ gb-web-site/src/Components/singleCase.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
-import "./supportCase.css";
+import "./singleCase.css";
 
 function getPriorityClass(priority) {
     if (priority >= 8) return "high";
